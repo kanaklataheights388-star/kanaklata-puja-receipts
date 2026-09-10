ebf860a5a71517771516a0017b922be617143c7b
kanaklataheights388-star <kanaklataheights388@gmail.com>
2026-09-10 12:30:59 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='puja-receipts-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest']))));
+const CACHE='puja-receipts-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./receipt-theme.css','./durga-receipt-background.png','./app.js','./manifest.webmanifest']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
