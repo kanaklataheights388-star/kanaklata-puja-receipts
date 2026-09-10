70e902c4700a4bf7ae314c5ba7fcdd2ab3c5d89f
kanaklataheights388-star <kanaklataheights388@gmail.com>
2026-09-10 16:32:50 +0530

    Remove duplicate receipt date

--- sw-v2.js
+++ sw-v2.js
@@ -1,5 +1,5 @@
-const CACHE='puja-receipts-v7';
-const ASSETS=['./','./index.html','./styles.css?v=6','./receipt-theme.css?v=6','./durga-receipt-background.png','./community-puja-moment.jpg','./app.js?v=6','./manifest.webmanifest'];
+const CACHE='puja-receipts-v8';
+const ASSETS=['./','./index.html','./styles.css?v=6','./receipt-theme.css?v=6','./durga-receipt-background.png','./community-puja-moment.jpg','./app.js?v=7','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).then(response=>response).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./')))));
